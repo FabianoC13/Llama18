cc4a911e96365e3b6416cc4a5c37522e747b0a1d
Fabiano <fabianocalvay@gmail.com>
2024-04-22 08:49:48 +0200

    Initial Commit

---
+++ Llama18/Llama18App.swift
@@ -0,0 +1,17 @@
+//
+//  Llama18App.swift
+//  Llama18
+//
+//  Created by Fabiano Calvay on 4/22/24.
+//
+
+import SwiftUI
+
+@main
+struct Llama18App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
